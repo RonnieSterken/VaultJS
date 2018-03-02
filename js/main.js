8d32ec4d9b23b147025c52af0ba431747faa696d
R Sterken <ronniesterken19@gmail.com>
2018-03-02 19:16:40 +0100

    added and updated
    updated java and css files
    added sounds
    fixed blinking
    added disabling buttons

--- js/main.js
+++ js/main.js
@@ -3,8 +3,34 @@ var numberContainer = document.getElementById("number-container");
 var correctCode = document.getElementById("correct-code");
 var wrongCode = document.getElementById("wrong-code");
 var showNumbers = document.getElementById("numbers");
-
+var showResult = document.getElementById("result");
+var numberButtons = document.getElementsByClassName("numberButton");
 var clickCounter = 0;
+var intervalTimerRight = 0;
+var intervalTimerFalse = 0;
+var accesAudio = new Audio('snd/Acces.mp3');
+var noAccesAudio = new Audio('snd/noAcces.mp3');
+
+
+
+console.log(numberButtons);
+
+function disableButtons() {
+
+	//used to loop through all buttons and disable them with attribute disable
+	//so that it isn't possible to click more then three times
+	for(i=0; i < numberButtons.length; i++) {
+		numberButtons[i].setAttribute('disabled', 'disabled');
+	}
+}
+
+function enableButtons() {
+
+	//used to loop through all buttons and enable them again, remove attribute disabled
+	for(i=0; i < numberButtons.length; i++) {
+		numberButtons[i].removeAttribute('disabled');
+	}
+}
 
 function getNumber(button) {
   console.log(button.value);
@@ -14,31 +40,67 @@ function getNumber(button) {
   console.log(clickCounter);
 
   if (clickCounter == 1) {
-      numberOne = button.value;
-  }
-  else if (clickCounter == 2) {
-      numberTwo = button.value;
-  }
-  else {
-      numberThree = button.value;
-      if (numberOne == 2 && numberTwo == 1 && numberThree == 3) {
-        result.innerHTML = "The code is right!";
+    numberOne = button.value;
+  } else if (clickCounter == 2) {
+    numberTwo = button.value;
+  } else {
+    numberThree = button.value;
+
+    disableButtons();
+
+    if (numberOne == 2 && numberTwo == 1 && numberThree == 3) {
+      showResult.innerHTML = "The code is right!";
+				accesAudio.play();
         showNumbers.innerHTML = "";
 
 
+        // document.getElementsByName("codeButton").disabled = true;
 
-      }
-      else {
-        result.innerHTML = "The code is wrong!";
+        var blink = setInterval(function() {
 
-        showNumbers.innerHTML = "";
+        intervalTimerRight++;
 
+        //turning div on and off
+        if (correctCode.style.visibility == 'visible') {
+          correctCode.style.visibility = 'hidden';
+        } else {
+          correctCode.style.visibility = 'visible';
+        }
 
-      }
-      clickCounter = 0;
+        //check if it blinked 10 times
+        if (intervalTimerRight == 11) {
 
+          //ClearInterval function stops the interval after 10 times
+          showResult = "";
+          intervalTimerRight = 1;
+          clearInterval(blink);
+          enableButtons();
+        }
+      }, 500);
+    } else {
+      result.innerHTML = "The code is wrong!";
+			noAccesAudio.play();
+      showNumbers.innerHTML = "";
+
+      var blink = setInterval(function() {
+        //add +1 every time the setinterval function runs
+        intervalTimerFalse++;
+        //method to show div on and off
+        if (wrongCode.style.visibility == 'visible') {
+          wrongCode.style.visibility = 'hidden';
+        } else {
+          wrongCode.style.visibility = 'visible';
+        }
+        //check if the interval has runned ten times
+        if (intervalTimerFalse == 11) {
+          //ClearInterval function stops the interval after 10 times
+          showResult = "";
+          intervalTimerFalse = 1;
+          clearInterval(blink);
+          enableButtons();
+        }
+      }, 500);
+    }
+    clickCounter = 0;
   }
-
-
-
 }
